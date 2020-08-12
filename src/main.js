0ec08cc1cadc9c341735f269634027a31bb94089
prof-H3645 <1009101437@qq.com>
2020-08-12 20:55:37 +0800

    profile基本实现

--- src/main.js
+++ src/main.js
@@ -5,16 +5,15 @@ import store from './store'
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import axios from 'axios'
-import qs from 'qs'
 
+import qs from 'qs'
+import Axios from 'axios'
+Vue.prototype.$axios = Axios;
 // axios配置
-axios.defaults.timeout = 5000 // 响应时间
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8' // 配置post请求头
-axios.defaults.headers.get['Content-Type'] = 'application/json;charset=UTF-8' // 配置get请求头
-axios.defaults.baseURL = '***' // 配置接口地址
+Axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8' // 配置post请求头
+Axios.defaults.headers.get['Content-Type'] = 'application/json;charset=UTF-8' // 配置get请求头
 // POST传参序列化(添加请求拦截器)
-axios.interceptors.request.use((config) => {
+Axios.interceptors.request.use((config) => {
     // 在发送请求之前做某件事
     if (config.method === 'post') {
         // 这里使用qs对数据进行处理，防止后台$_POST接收不到
@@ -26,8 +25,6 @@ axios.interceptors.request.use((config) => {
     return Promise.reject(error)
 })
 
-Vue.prototype.$http = axios // 定义axios的全局变量
-
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
@@ -37,3 +34,4 @@ new Vue({
     store,
     render: h => h(App)
 }).$mount('#app')
+
--- src/views/profile.vue
+++ src/views/profile.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="profile">
         <el-container>
+
             <el-header height='60px'>
                 <img class="header-logo left" src="../assets/logo.png" alt="logo">
                 <div class="header-info right">
@@ -9,8 +10,11 @@
                     <el-link type="danger" :underline="false">注册</el-link>&nbsp;
                 </div>
             </el-header>
+
             <el-main class="w" v-loading="loading">
                 <div class="form">
+
+                    <!-- 个人信息页面主表单 -->
                     <el-form ref="profile" :model="profile" label-width="100px">
                         <el-form-item label="用户编号：">
                             {{profile.uid}}
@@ -18,13 +22,13 @@
                         <el-form-item label="用户昵称：">
                             <el-input v-model="profile.uname" placeholder="请输入昵称"></el-input>
                         </el-form-item>
-                        <!-- 待处理 -->
                         <el-form-item label="密码：" class="pwd">
                             ******
                             <el-button type="text" class="right" @click="dialogVisible = true">修改密码</el-button>
                         </el-form-item>
                         <el-form-item label="年龄：">
-                            <el-input-number v-model.number="profile.age" placeholder="请输入年龄"></el-input-number>
+                            <el-input-number :min="1" v-model.number="profile.age" placeholder="请输入年龄">
+                            </el-input-number>
                         </el-form-item>
                         <el-form-item label="性别：">
                             <el-radio-group v-model="profile.gender">
@@ -36,38 +40,43 @@
                             <el-input type="textarea" resize="none" :rows="4" v-model="profile.introduction"></el-input>
                         </el-form-item>
                         <el-form-item label="兴趣爱好：">
-                            <el-input v-model="profile.hobby" placeholder="请输入昵称"></el-input>
+                            <el-input v-model="profile.hobby" placeholder="请输入爱好"></el-input>
                         </el-form-item>
                         <el-form-item label-width="0px">
-                            <el-button type="primary" @click="submitForm('profile')">保存</el-button>
+                            <el-button type="primary" @click="onSubmit()">保存</el-button>
                         </el-form-item>
                     </el-form>
+
+                    <!-- 修改密码弹出的对话框 -->
                     <el-dialog title="修改密码" :visible.sync="dialogVisible" width="400px">
-                        <el-form ref="password" :model="pwdtemp" :rules="rules">
+                        <el-form ref="password" :model="password" :rules="rules">
                             <el-form-item label="原密码" prop="formerPwd">
-                                <el-input type="password" v-model="pwdtemp.formerPwd" autocomplete="off">
+                                <el-input type="password" v-model="password.formerPwd" autocomplete="off">
                                 </el-input>
                             </el-form-item>
                             <el-form-item label="新密码" prop="newPwd">
-                                <el-input type="password" v-model="pwdtemp.newPwd" autocomplete="off">
+                                <el-input type="password" v-model="password.newPwd" autocomplete="off">
                                 </el-input>
                             </el-form-item>
                             <el-form-item label="新密码确认" prop="newPwdCheck">
-                                <el-input type="password" v-model="pwdtemp.newPwdCheck" autocomplete="off">
+                                <el-input type="password" v-model="password.newPwdCheck" autocomplete="off">
                                 </el-input>
                             </el-form-item>
-
                             <el-form-item>
                                 <el-button @click="dialogVisible = false">取 消</el-button>
-                                <el-button type="primary" @click="submitChange('password');dialogVisible = false">确 定
+                                <el-button type="primary" @click="dialogVisible = false;submitPassword()">确 定
                                 </el-button>
                             </el-form-item>
                         </el-form>
                     </el-dialog>
+
                 </div>
             </el-main>
+
             <el-footer>
+                <img src="../assets/footer.png" alt="footer">
             </el-footer>
+
         </el-container>
     </div>
 </template>
@@ -78,15 +87,16 @@
         const reg = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z_]{6,18}$/;
         return reg.test(str);
     }
-
     export default {
         name: 'profile',
         data() {
             var validatePass = (rule, value, callback) => {
                 if (value === "") {
                     callback(new Error("请输入原密码"));
+                } else if (value !== this.profile.pwd) {
+                    callback(new Error("密码错误"));
                 } else {
-                    if (this.pwdtemp.newPwd !== "") {
+                    if (this.password.newPwd !== "") {
                         this.$refs.password.validateField("newPwd");
                     }
                     callback();
@@ -99,20 +109,19 @@
                     callback(
                         new Error("密码长度在6~18之间，只能包含字母、数字和下划线中的至少两种")
                     );
-                } else if (value === this.pwdtemp.formerPwd) {
+                } else if (value === this.password.formerPwd) {
                     callback(new Error("新密码不能与原密码一致！"));
                 } else {
-                    if (this.pwdtemp.newPwdCheck !== "") {
+                    if (this.password.newPwdCheck !== "") {
                         this.$refs.password.validateField("newPwdCheck");
                     }
                     callback();
                 }
             };
-
             var validatePass2 = (rule, value, callback) => {
                 if (value === "") {
                     callback(new Error("请再次输入新密码"));
-                } else if (value !== this.pwdtemp.newPwd) {
+                } else if (value !== this.password.newPwd) {
                     callback(new Error("两次输入密码不一致！"));
                 } else {
                     callback();
@@ -120,15 +129,15 @@
             };
             return {
                 profile: {
-                    uid: '1837',
-                    uname: 'prof',
-                    pwd: 'testtest',
-                    age: 20,
-                    gender: 1,
-                    introduction: '我太菜了',
-                    hobby: '摸鱼',
+                    uid: '1',
+                    uname: 'g',
+                    pwd: '123abc',
+                    age: '',
+                    gender: '',
+                    introduction: '',
+                    hobby: '',
                 },
-                pwdtemp: {
+                password: {
                     formerPwd: '',
                     newPwd: '',
                     newPwdCheck: '',
@@ -157,27 +166,76 @@
         },
 
         methods: {
-            submitForm(formName) {
-                this.$refs[formName].validate((valid) => {
-                    if (valid) {
-                        alert('submit!');
-                    } else {
-                        console.log('error submit!!');
-                        return false;
+            submitPassword() {
+                var uid = this.profile.uid;
+                var newPwd = this.password.newPwd;
+                var formerPwd = this.password.formerPwd;
+                this.$axios({
+                    method: 'post',
+                    url: 'http://39.97.122.202/User/edit/' + uid,
+                    data: {
+                        formerPwd: formerPwd,
+                        newPwd: newPwd,
                     }
+                }).then(
+                    response => {
+                        var fmsg = '密码修改失败！';
+                        var rmsg = response.data.formerPwd;
+                        if (rmsg == formerPwd) alert('密码修改成功！');
+                        else {
+                            alert(fmsg);
+                            this.dialogVisible = true;
+                        }
+                    },
+                    err => {
+                        console.log(err);
+                    }).catch((error) => {
+                    console.log(error);
                 });
             },
-            submitChange(formName) {
-                this.$refs[formName].validate((valid) => {
-                    if (valid) {
-                        alert('new password submit!');
-                    } else {
-                        console.log('error submit!!');
-                        return false;
+            onSubmit() {
+                var uid = this.profile.uid;
+                var uname = this.profile.uname;
+                var age = this.profile.age;
+                var gender = this.profile.gender;
+                var introduction = this.profile.introduction;
+                var hobby = this.profile.hobby;
+                this.$axios({
+                    method: 'post',
+                    url: 'http://39.97.122.202/User/edit/' + uid,
+                    data: {
+                        name: uname,
+                        age: age,
+                        gender: gender,
+                        introduction: introduction,
+                        hobby: hobby,
                     }
+                }).then(
+                    response => {
+                        console.log(this.profile.uid);
+                        var fmsg = '保存失败，请重新提交！';
+                        var rmsg = response.data;
+                        if (rmsg == '成功') alert('保存成功！');
+                        else alert(fmsg);
+                    },
+                    err => {
+                        console.log(err);
+                    }).catch((error) => {
+                    console.log(error);
                 });
             },
-        }
+        },
+        created() {
+            this.$axios().then(
+                response => {
+                    this.profile = response.data;
+                },
+                err => {
+                    console.log(err);
+                }).catch((error) => {
+                console.log(error);
+            });
+        },
     }
 </script>
 
@@ -242,5 +300,9 @@
 
     .pwd {
         position: relative;
+    }
+
+    .el-footer img {
+        width: 100%;
     }
 </style>
