829ffa62441f448d8a88582a52dd170647585022
papermooon <63181559+papermooon@users.noreply.github.com>
2020-08-16 19:15:59 +0800

    退出团队小插件

---
+++ src/components/Gexit.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+            <span  @mouseover="shut" v-if="ishover">
+                        <el-button type="danger" icon="el-icon-error" ></el-button>
+            </span>
+
+                <span  @mouseout="overShow"  v-if="!ishover" >
+                    <el-button type="danger" round @click="push">退出团队</el-button>
+                </span>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Gexit',
+    props:{
+        groupID: {
+            type: Number,
+            default: () => 0
+            //这个群组的id；
+        },
+    },
+    data(){
+        return{
+            ishover:true,
+            myid:1,
+        }
+    },
+    methods:{
+            overShow(){
+          this.ishover=true;
+      },
+      shut(){
+          this.ishover=false;
+      },
+      push(){
+            this.$axios({
+            method:'post',
+            url:'http://39.97.122.202/group/quit_group/',
+            data:{
+              groupnum:this.groupID,
+              id:this.myid
+            }
+        }).then( res => {
+          if(res.data==1)
+            {
+                alert('操作成功！');
+            }
+            else if(res.data==2){
+                alert('错误，你不在这个团队里');
+            }
+          else
+            alert('咦，似乎发生了一点小错误..');
+        }).catch(err => {
+          console.log(err);
+          alert('当前网络状态不允许你退团的样子..');
+        })
+      },
+    },
+     mounted(){
+        if(localStorage.getItem('userID')!=null)
+            this.myid=localStorage.getItem('userID');
+        else{
+            alert('抓取本地用户id失败');
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>>
